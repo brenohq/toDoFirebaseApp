7ead8362a5a037901dfe91798e18435960c1ea10
Breno Henrique <brenosc2@hotmail.com>
2017-01-06 12:40:47 -0200

    initial commit

---
+++ index.android.js
@@ -0,0 +1,49 @@
+import React, {
+  Component
+} from 'react';
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyDIrhFQdsflU1HuTo0LBPme2sy45VaK26A",
+  authDomain: "todofirebaseapp-7e3dd.firebaseapp.com",
+  databaseURL: "https://todofirebaseapp-7e3dd.firebaseio.com",
+  storageBucket: "todofirebaseapp-7e3dd.appspot.com",
+  messagingSenderId: "863948846414"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+database.ref().set({
+  nome: 'Breno'
+});
+
+export default class todoFirebaseApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
+
+AppRegistry.registerComponent('todoFirebaseApp', () => todoFirebaseApp);
