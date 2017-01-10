c4e6e5c6e1ebf7ffed58d44281010a8fb528537f
Breno Henrique <brenosc2@hotmail.com>
2017-01-10 00:17:54 -0200

    app working but problems with color prop type

--- index.android.js
+++ index.android.js
@@ -6,7 +6,10 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  ListView,
+  TouchableHighlight
 } from 'react-native';
 
 import * as firebase from 'firebase';
@@ -20,32 +23,159 @@ const config = {
 };
 firebase.initializeApp(config);
 
-const database = firebase.database();
-const databaseRef = database.ref();
-
-databaseRef.set({
-  nome: 'Breno Henrique'
-});
-
 export default class todoFirebaseApp extends Component {
+  constructor(props) {
+    super(props);
+    const database = firebase.database();
+    const myFirebaseRef = database.ref();
+    this.itemsRef = myFirebaseRef.child('items');
+
+    this.state = {
+      newTodo: '',
+      todoSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2
+      })
+    }
+
+    this.items = [];
+  }
+
+  componentDidMount() {
+    this.itemsRef.on('child_added', (dataSnapshot) => {
+      this.items.push({
+        id: dataSnapshot.key,
+        text: dataSnapshot.val()
+      });
+      this.setState({
+        todoSource: this.state.todoSource.cloneWithRows(this.items)
+      });
+    });
+
+    this.itemsRef.on('child_removed', (dataSnapshot) => {
+      this.items = this.items.filter((x) => x.id !== dataSnapshot.key);
+      this.setState({
+        todoSource: this.state.todoSource.cloneWithRows(this.items)
+      });
+    });
+  }
+
+  addTodo() {
+    if (this.state.newTodo !== '') {
+      this.itemsRef.push({
+        todo: this.state.newTodo
+      });
+      this.setState({
+        newTodo: ''
+      });
+    }
+  }
+
+  removeTodo(rowData) {
+    this.itemsRef.child(rowData.id).remove();
+  }
+
+
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View style={styles.appContainer}>
+        <View style={styles.titleView}>
+          <Text style={styles.titleText}>
+            My Todos
+          </Text>
+        </View>
+        <View style={styles.inputcontainer}>
+          <TextInput style={styles.input} onChangeText={(text) => this.setState({newTodo: text})} value={this.state.newTodo}/>
+          <TouchableHighlight
+            style={styles.button}
+            onPress={() => this.addTodo()}
+            underlayColor='#dddddd'>
+            <Text style={styles.btnText}>Add!</Text>
+          </TouchableHighlight>
+        </View>
+        <ListView
+          enableEmptySections={true}
+          dataSource={this.state.todoSource}
+          renderRow={this.renderRow.bind(this)} />
       </View>
+    );
+  }
+
+  renderRow(rowData) {
+    return (
+      <TouchableHighlight
+        underlayColor='#dddddd'
+        onPress={() => this.removeTodo(rowData)}>
+        <View>
+          <View style={styles.row}>
+            <Text style={styles.todoText}>{rowData.text.todo}</Text>
+          </View>
+          <View style={styles.separator} />
+        </View>
+      </TouchableHighlight>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+var styles = StyleSheet.create({
+  appContainer: {
+    flex: 1
+  },
+  titleView: {
+    backgroundColor: '#48afdb',
+    paddingTop: 30,
+    paddingBottom: 10,
+    flexDirection: 'row'
+  },
+  titleText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    flex: 1,
+    fontSize: 20,
+  },
+  inputcontainer: {
+    marginTop: 5,
+    padding: 10,
+    flexDirection: 'row'
+  },
+  button: {
+    height: 36,
+    flex: 2,
+    flexDirection: 'row',
+    backgroundColor: '#48afdb',
+    justifyContent: 'center',
+    color: '#fff',
+    borderRadius: 4,
+  },
+  btnText: {
+    fontSize: 18,
+    color: '#fff',
+    marginTop: 6,
+  },
+  input: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flex: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48afdb',
+    borderRadius: 4,
+    color: '#48BBEC'
+  },
+  row: {
+    flexDirection: 'row',
+    padding: 12,
+    height: 44
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#CCCCCC',
+  },
+  todoText: {
+    flex: 1,
+  }
+});
 
 AppRegistry.registerComponent('todoFirebaseApp', () => todoFirebaseApp);
